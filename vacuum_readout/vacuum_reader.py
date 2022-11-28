480fa585700dc705b78796c7e78c9ba1c3deb155
Alek Gajos <alek.gajos@gmail.com>
2022-11-28 11:32:07 +0100

    Update for USB vacuum readout

--- vacuum_readout/vacuum_reader.py
+++ vacuum_readout/vacuum_reader.py
@@ -5,7 +5,12 @@ from math import nan
 from datetime import datetime
 import socket
 import signal
+import select
 import re
+import json
+
+USB_DEVICE = '/dev/ttyUSB0'
+PORT_NUMBER = 5143
 
 sensor_status = {
     0 : "Measurement data OK",
@@ -28,12 +33,8 @@ def parseGaugeData(data):
         logging.error("Pressure readout decoding error: {} for data: {}".format(str(err), data))
         return (0.0, 7)
 
-#    data = data.split("\r")[0]
-
-    
     try:
         status = int(data.split(",")[0])
-#        data = data.split(",")[1]
         data = re.findall("\d+\.\d+E[+5D-]\d+", data)
         data = data[0]
         readout = float(data)
@@ -45,7 +46,7 @@ def parseGaugeData(data):
 
 def readGauge(gauge_no):
     
-    serial_port = serial.Serial('/dev/ttyS1', 9600, timeout=1)
+    serial_port = serial.Serial(USB_DEVICE, 9600, timeout=1)
     query = "PR{}\r\n".format(gauge_no)
 
     # try to send query until ACK is received
@@ -79,82 +80,66 @@ def readGauge(gauge_no):
 
     return reading
 
-def readMeteoData():
-    
-    serial_port = serial.Serial('/dev/ttyUSB0', 19200, parity=serial.PARITY_NONE, timeout=10)
-    query = b'Q'
-    serial_port.write(query)
-    sleep(0.2)
-    
-    is_data = True
-    data = []
-    
-    while is_data:
-        payload = serial_port.readline()
-        if len(payload) == 0:
-            is_data = False
-        else:
-            payload = payload.decode('utf-8').strip()
-            data.append(payload)
-    
-    serial_port.close()
-    
-    return data
-
-def formatMeteoData(meteo_data, vacuum_data):
+def formatData(pressure_data):
     current_time = datetime.now()
-    current_time.strftime("%Y-%m-%d %H:%M:%S")
-    return_string = current_time.strftime("%Y-%m-%d %H:%M:%S") + " > "
+    timestamp = current_time.strftime("%Y-%m-%dT%H:%M:%S")
 
-    # use a dummy data string for now
-    meteo_data[10].replace("H1", "H0")
-    meteo_data[11].replace("H2", "H1")
+    data = {
+            "MEASUREMENT_TIME" : timestamp,
+            "P1" : {
+                "value" : pressure_data[0],
+                "unit" : "Pa"
+                },
+            "P2" : {
+                "value" : pressure_data[1],
+                "unit" : "Pa"
+                }
 
-    meteo_string = "; ".join(meteo_data)
-    #meteo_string = "#0: 24.90; #1: 22.20; #2: 19.40; #3: 20.50; #4: 27.90; #5: 20.20; #6: 19.20; #7: 22.10; #8: 25.10; #9: 22.90; H0: 1.00; H1: 25.80; P: 99037.00 Pa;"
+            }
 
-    return_string += meteo_string
-    return_string += "P1: {} mbar; P2: {} mbar;".format(*vacuum_data)
-
-    return return_string
+    return json.dumps(data)
 
 if __name__ == "__main__":
 
+    should_continue = True
+    
     # register SIGINT handler to exit gracefully on Ctrl-C
     def signal_handler(sig, frame):
+        global should_continue
         logging.info("Ctrl-C/SIGINT received, exiting.")
-        sock.close()        
-
+        should_continue = False
+        
     signal.signal(signal.SIGINT, signal_handler)
 
-    logging.basicConfig(filename='sensors.log',level=logging.DEBUG,
+    logging.basicConfig(filename='vacuum_readout.log',level=logging.DEBUG,
                         format = '%(asctime)s - %(levelname)s: %(message)s',
                         datefmt = '%m/%d/%Y %I:%M:%S %p')
-
+    
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.settimeout(None)
-    sock.bind(('', 5143))
+    sock.bind(('', PORT_NUMBER))
 
-    while True:
-        query, client_address = sock.recvfrom(1024)
-        if query == b'Q':
-            logging.info("received data query")
+    epoll = select.epoll()
+    epoll.register(sock.fileno(), select.EPOLLIN)
 
-            meteo_data = readMeteoData()
+    while should_continue:
 
-            try:
-                pressure_data = (readGauge(1), readGauge(2))
-            except Exception as err:
-                logging.error("Error reading gauges: {}".format(str(err)))
-                pressure_data = (0, 0)
+        events = epoll.poll(1)
+        for fileno, event in events:
+            if fileno == sock.fileno():
 
-            print(pressure_data[0])
-            print(pressure_data[1])        
-            data = bytearray(formatMeteoData(meteo_data, pressure_data), 'utf-8') 
-            sock.sendto(data, client_address)
-            logging.info("data has been sent")
-            
-#    data = bytearray(formatMeteoData(readMeteoData(), (readGauge(1), readGauge(2))), 'utf-8') 
-#    print(data)
-    
+                query, client_address = sock.recvfrom(1024)
+
+                try:
+                    pressure_data = (readGauge(1), readGauge(2))
+                except Exception as err:
+                    logging.error("Error reading gauges: {}".format(str(err)))
+                    pressure_data = (0, 0)
+
+                logging.info("Presure data: " + str(pressure_data[0]) + ", " + str(pressure_data[1])) 
+
+                data = bytearray(formatData(pressure_data), 'utf-8') 
+                sock.sendto(data, client_address)
+                logging.info("data has been sent")
+
     sock.close()

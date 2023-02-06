fa417a8299fb22c18a775a154223567295dfffa1
BuildTools <unconfigured@null.spigotmc.org>
2023-02-06 22:05:39 +0100

    Reformat

--- vacuum_readout/vacuum_reader.py
+++ vacuum_readout/vacuum_reader.py
@@ -1,36 +1,39 @@
-import serial
-from time import sleep
-import logging
-from math import nan
-from datetime import datetime
-import socket
-import signal
-import select
-import re
 import json
+import logging
+import re
+import select
+import signal
+import socket
+from datetime import datetime
+from math import nan
+from time import sleep
 
-USB_DEVICE = '/dev/ttyUSB0'
+import serial
+
+USB_DEVICE = "/dev/ttyUSB0"
 PORT_NUMBER = 5143
 
 sensor_status = {
-    0 : "Measurement data OK",
-    1 : "Underrange",
-    2 : "Overrange",
-    3 : "Sensor error",
-    4 : "Sensor off",
-    5 : "No sensor",
-    6 : "Identification error",
-    7 : "Unable to decode readout"
+    0: "Measurement data OK",
+    1: "Underrange",
+    2: "Overrange",
+    3: "Sensor error",
+    4: "Sensor off",
+    5: "No sensor",
+    6: "Identification error",
+    7: "Unable to decode readout",
 }
 
-def parseGaugeData(data):
 
+def parseGaugeData(data):
     # data looks like b'0,+7.8900E+02\rx15\r'
-    
+
     try:
-        data = data.decode('ascii', 'ignore')
+        data = data.decode("ascii", "ignore")
     except UnicodeDecodeError as err:
-        logging.error("Pressure readout decoding error: {} for data: {}".format(str(err), data))
+        logging.error(
+            "Pressure readout decoding error: {} for data: {}".format(str(err), data)
+        )
         return (0.0, 7)
 
     try:
@@ -39,28 +42,34 @@ def parseGaugeData(data):
         data = data[0]
         readout = float(data)
     except Exception as err:
-        logging.error("Status or pressure decoding error: {} for data: {}".format(str(err), data))
+        logging.error(
+            "Status or pressure decoding error: {} for data: {}".format(str(err), data)
+        )
         return (0.0, 7)
-        
+
     return (readout, status)
 
+
 def readGauge(gauge_no):
-    
     serial_port = serial.Serial(USB_DEVICE, 9600, timeout=1)
     query = "PR{}\r\n".format(gauge_no)
 
     # try to send query until ACK is received
     attempt_counter = 0
     while True:
-        serial_port.write(bytearray(query, 'ascii'))
+        serial_port.write(bytearray(query, "ascii"))
         sleep(0.2)
         response = serial_port.readline()
         attempt_counter = attempt_counter + 1
-        if response == b'\x06\r\n':
+        if response == b"\x06\r\n":
             print("ACK received after {} attempts.".format(attempt_counter))
             break
         if attempt_counter > 9:
-            raise Exception("Response for the gauge {} is not ACK after 10th attempt. Received response: {}.".format(gauge_no, response))
+            raise Exception(
+                "Response for the gauge {} is not ACK after 10th attempt. Received response: {}.".format(
+                    gauge_no, response
+                )
+            )
 
     # at this stage we should be ready to receive data
     sleep(0.2)
@@ -70,64 +79,61 @@ def readGauge(gauge_no):
     sleep(0.2)
     response = serial_port.readline()
     serial_port.close()
-    
+
     # parse the retuned data
     reading, status = parseGaugeData(response)
 
     if status != 0:
-        logging.warning("Pressor sensor {} error status: {}".format(gauge_no, sensor_status[status]))
+        logging.warning(
+            "Pressor sensor {} error status: {}".format(gauge_no, sensor_status[status])
+        )
         reading = nan
 
     return reading
+
 
 def formatData(pressure_data):
     current_time = datetime.now()
     timestamp = current_time.strftime("%Y-%m-%dT%H:%M:%S")
 
     data = {
-            "MEASUREMENT_TIME" : timestamp,
-            "P1" : {
-                "value" : pressure_data[0],
-                "unit" : "Pa"
-                },
-            "P2" : {
-                "value" : pressure_data[1],
-                "unit" : "Pa"
-                }
-
-            }
+        "MEASUREMENT_TIME": timestamp,
+        "P1": {"value": pressure_data[0], "unit": "Pa"},
+        "P2": {"value": pressure_data[1], "unit": "Pa"},
+    }
 
     return json.dumps(data)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     should_continue = True
-    
+
     # register SIGINT handler to exit gracefully on Ctrl-C
     def signal_handler(sig, frame):
         global should_continue
         logging.info("Ctrl-C/SIGINT received, exiting.")
         should_continue = False
-        
+
     signal.signal(signal.SIGINT, signal_handler)
 
-    logging.basicConfig(filename='vacuum_readout.log',level=logging.DEBUG,
-                        format = '%(asctime)s - %(levelname)s: %(message)s',
-                        datefmt = '%m/%d/%Y %I:%M:%S %p')
-    
+    logging.basicConfig(
+        filename="vacuum_readout.log",
+        level=logging.DEBUG,
+        format="%(asctime)s - %(levelname)s: %(message)s",
+        datefmt="%m/%d/%Y %I:%M:%S %p",
+    )
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.settimeout(None)
-    sock.bind(('', PORT_NUMBER))
+    sock.bind(("", PORT_NUMBER))
 
     epoll = select.epoll()
     epoll.register(sock.fileno(), select.EPOLLIN)
 
     while should_continue:
-
         events = epoll.poll(1)
         for fileno, event in events:
             if fileno == sock.fileno():
-
                 query, client_address = sock.recvfrom(1024)
 
                 try:
@@ -136,9 +142,14 @@ if __name__ == "__main__":
                     logging.error("Error reading gauges: {}".format(str(err)))
                     pressure_data = (0, 0)
 
-                logging.info("Presure data: " + str(pressure_data[0]) + ", " + str(pressure_data[1])) 
+                logging.info(
+                    "Presure data: "
+                    + str(pressure_data[0])
+                    + ", "
+                    + str(pressure_data[1])
+                )
 
-                data = bytearray(formatData(pressure_data), 'utf-8') 
+                data = bytearray(formatData(pressure_data), "utf-8")
                 sock.sendto(data, client_address)
                 logging.info("data has been sent")
 
